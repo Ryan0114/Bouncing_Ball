69934c13da95082db5b9f15366d65a3474445970
Ryan Huang <pi88031221@gmail.com>
2025-05-24 19:15:23 +0800

    add main page and level selection page (not styled yet)

--- src/com/binge/Character.java
+++ src/com/binge/Character.java
@@ -1,5 +1,6 @@
 package com.binge;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Character {
@@ -10,16 +11,16 @@ public class Character {
     boolean movingLeft, movingRight, movingUp, specialTransport;
     int coins;
 
-    Character(double posX, double posY, int radius, double mass) {
+    Character(double posX, double posY, int radius, Color color) {
         this.pos = new Point2D(posX, posY);
         this.v = new Point2D(0, 0);
         this.radius = radius;
-        this.mass = mass;
         this.movingLeft = false;
         this.movingRight = false;
         this.movingUp = false;
         this.coins = 0;
         this.body = new Circle(posX, posY, radius);
+        this.body.setFill(color);
         this.specialTransport = false;
         this.jumpCount = 0;
     }
